cd759248eb2e9230708899274da1811eedaa9916
JackTam <371997352@qq.com>
2016-12-21 17:13:40 +0800

    add bindonce to test the difference of number of watchers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,16 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['pasvaz.bindonce'])
 
 .controller('DashCtrl', function($scope, $http, Chats) {
-  $scope.thisLeagueInfo = Chats.getYingchaoData('英超');
-  var promise = $scope.thisLeagueInfo;
-  var realData;
-  promise.then(function (data) {
-    realData = data.data;
-    console.log(realData.result);
-    $scope.tabs = realData.result;
-  });
+    $scope.thisLeagueInfo = Chats.getYingchaoData('英超');
+    var promise = $scope.thisLeagueInfo;
+    var realData;
+    promise.then(function (data) {
+      realData = data.data;
+      console.log(realData.result);
+      $scope.tabs = realData.result;
+    }, function (error) {
+      console.log(error);
+    });
 })
 
 .controller('ChatsCtrl', function($scope, $http, Chats, $state) {
@@ -16,18 +18,18 @@ angular.module('starter.controllers', [])
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
-  $scope.thisLeagueInfo = Chats.getYingchaoData('西甲');
-  var promise = $scope.thisLeagueInfo;
-  var realData;
-  promise.then(function (data) {
-    realData = data.data;
-    console.log(realData.result);
-    $scope.tabs = realData.result;
-  });
+    $scope.thisLeagueInfo = Chats.getYingchaoData('西甲');
+    var promise = $scope.thisLeagueInfo;
+    var realData;
+    promise.then(function (data) {
+      realData = data.data;
+      console.log(realData.result);
+      $scope.tabs = realData.result;
+
+    }, function (error) {
+      console.log(error);
+    });
 
    $scope.leagueName = Chats.fiveLeagueName;
 
@@ -45,38 +47,105 @@ angular.module('starter.controllers', [])
       console.log(realData.result);
       $scope.tabs = realData.result.tabs;
 
+    },function (error) {
+      console.log(error);
     });
 
 })
 
 .controller('AccountCtrl', function($scope, $stateParams, Chats) {
-  $scope.thisLeagueInfo = Chats.getYingchaoData('意甲');
-  var promise = $scope.thisLeagueInfo;
-  var realData;
-  promise.then(function (data) {
-    realData= data.data;
-    console.log(realData.result);
-    $scope.tabs = realData.result;
 
-  });
+    $scope.thisLeagueInfo = Chats.getYingchaoData('意甲');
+    var promise = $scope.thisLeagueInfo;
+    var realData;
+    promise.then(function (data) {
+      realData = data.data;
+      console.log(realData.result);
+      $scope.tabs = realData.result;
+
+    }, function (error) {
+      console.log(error);
+    });
+
 })
 .controller('germanCtrl',function ($scope,$stateParams,Chats) {
-  $scope.thisLeagueInfo = Chats.getYingchaoData('德甲');
-  var promise = $scope.thisLeagueInfo;
-  var realData;
-  promise.then(function (data) {
-    realData = data.data;
-    console.log(realData.result);
-    $scope.tabs = realData.result;
-  });
+    $scope.thisLeagueInfo = Chats.getYingchaoData('德甲');
+    var promise = $scope.thisLeagueInfo;
+    var realData;
+    promise.then(function (data) {
+      realData = data.data;
+      console.log(realData.result);
+      $scope.tabs = realData.result;
+
+    }, function (error) {
+      console.log(error);
+    });
 })
 .controller('franceCtrl',function ($scope,$stateParams,Chats) {
-  $scope.thisLeagueInfo = Chats.getYingchaoData('法甲');
-  var promise = $scope.thisLeagueInfo;
-  var realData;
-  promise.then(function (data) {
-    realData = data.data;
-    console.log(realData.result);
-    $scope.tabs = realData.result;
-  });
+    $scope.thisLeagueInfo = Chats.getYingchaoData('法甲');
+    var promise = $scope.thisLeagueInfo;
+    var realData;
+    promise.then(function (data) {
+      realData = data.data;
+      console.log(realData.result);
+      $scope.tabs = realData.result;
+
+    }, function (error) {
+      console.log(error);
+    });
+
+  $scope.photoErr = false;
+  $scope.photoBtnDiable = true;
+  var mediaStream = null,track = null;
+
+  navigator.getMedia = (navigator.getUserMedia ||
+  navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia);
+  if (navigator.getMedia) {
+    navigator.getMedia(
+      {
+        video: true
+      },
+      // successCallback
+      function (stream) {
+        var s = window.URL.createObjectURL(stream);
+        var video = document.getElementById('video');
+        video.src = s;
+        mediaStream = stream;
+        track = stream.getTracks()[0];
+        $scope.photoBtnDiable = false;
+        $scope.$apply();
+      },
+      // errorCallback
+      function (err) {
+        //$scope.errorPhoto();
+        console.log("The following error occured:" + err);
+      });
+  } else {
+    $scope.errorPhoto();
+  }
+  $scope.snap = function () {
+    var canvas = document.getElementById('canvas');
+    context = canvas.getContext("2d");
+    canvas.width = "400";
+    canvas.height = "304";
+
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, 400, 304);
+    $scope.closeCamera();
+  };
+  $scope.closeCamera = function () {
+    if (mediaStream != null) {
+      if (mediaStream.stop) {
+        mediaStream.stop();
+      }
+      $scope.videosrc = "";
+    }
+    if (track != null) {
+      if (track.stop) {
+        track.stop();
+      }
+    }
+  }
+
 });
--- www/js/services.js
+++ www/js/services.js
@@ -6,11 +6,11 @@ angular.module('starter.services', [])
 
   var getYingchaoData = function (leagueName) {
     var url = 'http://op.juhe.cn/onebox/football/league?callback=JSON_CALLBACK&key=d636939fc762563f313988a647c36c63&league='+leagueName;
-    return $http.jsonp(url)
-      .then(function (data) {
-      //console.log(data);
-        return data;
-    })
+      return $http.jsonp(url)
+        .then(function (data) {
+          //console.log(data);
+          return data;
+        })
   };
 
     var fiveLeagueName= [{name:'英超'},{name:'西甲'},{name:'德甲'},{name:'意甲'},{name:'法甲'}];
